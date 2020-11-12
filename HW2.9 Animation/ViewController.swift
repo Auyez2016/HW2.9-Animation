3c4ca3ea138f3d1b52f816a0ba5e369278400099
Auyez <auyez2016@yandex.ru>
2020-11-12 16:00:51 +0600

    final

--- HW2.9 Animation/ViewController.swift
+++ HW2.9 Animation/ViewController.swift
@@ -5,15 +5,34 @@
 //  Created by Auyez on 11/11/20.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    @IBOutlet var springView: SpringView!
+    @IBOutlet var springViewLabel: SpringLabel!
+    @IBOutlet var springButton: SpringButton!
+    
+    var animationName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animationName = animation.init().preset
+        springButton.titleLabel?.text = animationName
+        springViewLabel.text = animationName
     }
-
-
+    
+    @IBAction func springButtonAction() {
+        
+        
+        springView.animation = animationName
+        springView.delay = 0.5
+        springView.animate()
+        
+        springViewLabel.text = animationName
+        springButton.titleLabel?.text = animationName
+        springButton.animation = animation.init().preset
+        springButton.animate()
+        animationName = animation.init().preset
+    }
 }
 
---
+++ Pods/Target Support Files/Pods-HW2.9 Animation/Pods-HW2.9 Animation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HW2_9_Animation : NSObject
+@end
+@implementation PodsDummy_Pods_HW2_9_Animation
+@end
